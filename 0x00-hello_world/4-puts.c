bf846015019804962783be4e6b3270e3eb029696
Ruth Njeri <rthmbugua@gmail.com>
2021-02-04 00:52:47 +0000

    change file to use puts function instead of putc

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,14 +3,7 @@
 
 int main()
 {
-	char array_string[] = 
-	{ 
-		'P', 'r', 'o', 'g', 'r', 'a', 'm', 'm', 'i', 'n', 'g', ' ',
-	       	'i', 's', ' ', 'l', 'i', 'k', 'e', ' ',
-     		'b', 'u', 'i', 'd', 'i', 'n', 'g', ' ',	'a', ' ',
-		'm', 'u', 'l', 't', 'i', 'l', 'i', 'n', 'g', 'u', 'a', 'l', 
-		' ', 'p', 'u', 'z', 'z', 'l', 'e', '\0'
-	};	
-	putc(array_string, stdout);		
+	char str[] = "Programming is like building a muiltilingual puzzle";
+	puts(str);	
 	return 0;
 }
