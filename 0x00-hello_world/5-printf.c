668855166f10badf4c2799bfa2f0ba1884113100
Ruth Njeri <rthmbugua@gmail.com>
2021-02-04 02:21:12 +0000

    add file to print string using pritf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+  * main - prints a string using printf
+  * @void: takes no parameters
+  *
+  * Return: 0 upon completion
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
