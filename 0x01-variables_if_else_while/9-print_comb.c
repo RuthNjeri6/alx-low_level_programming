696721e117e9c57d8e182f51e0f6a5718ef22165
Ruth Njeri <rthmbugua@gmail.com>
2021-02-05 13:39:09 +0000

    requirement error fix

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,8 +13,8 @@ int main(void)
 	{
 		for (j = 0; j < 10; j++)
 		{
-		        putchar(i+'0'); 	
-			putchar(j+'0');
+			putchar(i + '0');
+			putchar(j + '0');
 			putchar(',');
 			putchar(' ');
 		}
