073009562cb80097721fd3921e74e66004a37830
Ruth Njeri <rthmbugua@gmail.com>
2021-02-08 16:38:45 +0000

    fix styling errors

--- 0x02-functions_nested_loop/0-holberton.c
+++ 0x02-functions_nested_loop/0-holberton.c
@@ -1,17 +1,23 @@
 /**
-  * main - function the entry point
-  * @void: no parametors given
+  * _putchar - declaration
+  * @char: takes in a character argument
   *
-  * Return: upon sucess 0
-  */
-int _putchar(char c);
+  * Return: an integer retun value
+ */
+int _putchar(char);
 
+/**
+  * main -function the entry point
+  * @void: takes no parametors
+  *
+  * Return: 0 upon success
+  */
 int main(void)
 {
 	char arry_str[] = "Holberton";
 	int i = 0;
 
-	while(arry_str[i] != '\0')
+	while (arry_str[i] != '\0')
 	{
 		_putchar(arry_str[i]);
 		i++;
