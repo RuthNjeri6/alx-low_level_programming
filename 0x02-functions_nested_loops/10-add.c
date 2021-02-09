4edede3810d9d1511d5c3b0860eefb2ab19afb12
Ruth Njeri <rthmbugua@gmail.com>
2021-02-09 15:10:20 +0000

    add function to add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+  * add - adds to integers
+  * @a: first argument
+  * @b: second argument
+  *
+  * Return: the sum of two numbers
+  */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
