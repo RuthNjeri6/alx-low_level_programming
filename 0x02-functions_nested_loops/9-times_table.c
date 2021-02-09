c03f9e515213a09027d41e0c9e77a5ccb6e67028
Ruth Njeri <rthmbugua@gmail.com>
2021-02-09 14:46:16 +0000

    add function that computes the multiplication table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,46 @@
+#include "holberton.h"
+/**
+  * times_table - prnts the multiplication table upto 9
+  * @void: no parameters
+  *
+  * Return: nothing
+  */
+void times_table(void)
+{
+	int i, j, ans;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			ans = i * j;
+			if (ans >= 10 && j == 9)
+			{
+				_putchar(' ');
+				_putchar((ans / 10) + '0');
+				_putchar((ans % 10) + '0');
+			}
+			else if ((ans >= 10) && (j != 9))
+			{
+				_putchar(' ');
+				_putchar((ans / 10) + '0');
+				_putchar((ans % 10) + '0');
+				_putchar(',');
+			}
+			else if ((ans < 10) && (j == 9))
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar((ans % 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar((ans % 10) + '0');
+				_putchar(',');
+			}
+		}
+		_putchar('\n');
+	}
+}
