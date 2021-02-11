1b5269be5aaf2df292a7c8682a292b4a8061254a
Ruth Njeri <rthmbugua@gmail.com>
2021-02-11 13:45:06 +0000

    add function that checks is a letter is uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+/**
+  * _isupper - checks if a letter is uppercase
+  * @c: takes an integer variable
+  *
+  * Reurn: 1 if uppercase, 0 otherwise
+  */
+int _isupper(int c)
+{
+	int result;
+
+	if ((c >= 'A') && (c <= 'Z'))
+	{
+		result = 1;
+	}
+	else
+	{
+		result = 0;
+	}
+	return (result);
+}
