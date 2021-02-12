ff793ef89a232d200c892e1b9e4dd35106a531d8
Ruth Njeri <rthmbugua@gmail.com>
2021-02-12 03:08:02 +0000

    add function to drop triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+/**
+  * print_triangle - prints triangle
+  * @size: take size as an argument
+  *
+  * Return: nothing
+  */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = i; j < size; j++)
+			{
+				_putchar(' ');
+			}
+			for (j = 0; j <= i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
