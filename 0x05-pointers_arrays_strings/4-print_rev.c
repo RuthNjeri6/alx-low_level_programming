90a518df33cedd30ea4153739f7c043bef95d4a4
rthmbugua@gmail.com <rthmbugua@gmail.com>
2021-02-16 14:25:48 +0000

    add function to output a tring the reverse order

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+  * print_rev - print string in reverse
+  * @s: argument
+  *
+  * Return: void
+  */
+void print_rev(char *s)
+{
+	int length, i;
+	char *tmp;
+
+	tmp = s;
+	length = 0;
+
+	while (*s != 0)
+	{
+		length++;
+		s++;
+	}
+
+	for (i = length; i >= 0; i--)
+	{
+		_putchar(*(tmp + i));
+	}
+	_putchar('\n');
+}
