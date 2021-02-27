edabd2b1eb817dec9066f9ecadc8b8021b07bd0b
rthmbugua@gmail.com <rthmbugua@gmail.com>
2021-02-27 05:35:28 +0000

    add function that returns factorial of anumber

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+/**
+  * factorial - returns the factorial of a number
+  * @n: integer
+  *
+  * Return: integer
+  */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0 || n == 1)
+		return (1);
+	else
+		return (n * factorial(n - 1));
+}
