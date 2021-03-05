2aff8c7d86811cf115a041affdf8d47ea4cda6c9
rthmbugua@gmail.com <rthmbugua@gmail.com>
2021-03-05 09:35:24 +0000

    fix over allocation of memory

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,7 +13,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	ptr = malloc(sizeof(char) * (size + 1));
+	ptr = malloc(sizeof(char) * size);
 	if (ptr == 0)
 		return (NULL);
 	i = 0;
