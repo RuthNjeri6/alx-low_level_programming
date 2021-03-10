b8da178cda07e02c40b900b898edcc8453a5ddc9
rthmbugua@gmail.com <rthmbugua@gmail.com>
2021-03-10 08:04:16 +0000

    add function to free memory allocated to a struct

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+  * free_dog - frees memory allocated to a struct
+  * @d: pointer to struct
+  *
+  * Return: none
+  */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+		free(d);
+}
