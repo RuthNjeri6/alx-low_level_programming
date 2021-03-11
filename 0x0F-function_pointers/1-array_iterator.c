95c405b3a67bc66656d2204884f9b1228a4195b1
rthmbugua@gmail.com <rthmbugua@gmail.com>
2021-03-11 05:49:54 +0000

    fixed function to cater for null function pointer arguments

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,6 +11,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	static unsigned int i;
 
+	if (array == 0 || size <= 0 || action == 0)
+		return;
+
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
