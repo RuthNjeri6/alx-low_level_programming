5cac7ade8b4185f187daacba21e14a9dc9c8c629
rthmbugua@gmail.com <rthmbugua@gmail.com>
2021-03-25 14:25:20 +0000

    double inclusion

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,6 +1,8 @@
 #ifndef _HOLBERTON_H_
-#define _HOLBERTON_H
+#define _HOLBERTON_H_
+
 unsigned int binary_to_uint(const char *b);
 int _strlen(const char *b);
 int _pow(int a, int b);
+
 #endif /* _HOLBERTON_H_ */
