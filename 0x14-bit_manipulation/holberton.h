6045271b1af8fa06e5e5af7504aa5ca3bae9e284
rthmbugua@gmail.com <rthmbugua@gmail.com>
2021-03-25 20:47:00 +0000

    added function that checks the endianness

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -5,6 +5,7 @@ unsigned int binary_to_uint(const char *b);
 int _strlen(const char *b);
 int _putchar(char c);
 int _pow(int a, int b);
+int get_endianness(void);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
