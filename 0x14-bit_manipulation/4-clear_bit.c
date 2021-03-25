aa0071598b93ac1e6354669b55d8783b4266f54a
rthmbugua@gmail.com <rthmbugua@gmail.com>
2021-03-25 19:09:40 +0000

    added function that clears nth bit of a number to 0

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+  * clear_bit - clear nth bit to 0
+  * @n: points to number
+  * @index: index of bit
+  *
+  * Return: int
+  */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= 32)
+		return (-1);
+	*n = *n & ~(1 << index);
+	return (*n);
+}
