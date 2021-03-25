a08a8021758fb5e09d6b42fbf67aecd67ddae19b
rthmbugua@gmail.com <rthmbugua@gmail.com>
2021-03-25 20:34:33 +0000

    adde function that converts decimal to binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+  * print_binary - prints binary representation of a decimal number
+  * @n: decimal number
+  *
+  * Return: void
+  */
+void print_binary(unsigned long int n)
+{
+	if (n > 0)
+		print_binary(n >> 1);
+	_putchar('0' + (n & 1));
+}
