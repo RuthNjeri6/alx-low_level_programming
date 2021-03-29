b88b92c09431cb27357aa81ef766e4f2c4b90f60
rthmbugua@gmail.com <rthmbugua@gmail.com>
2021-03-29 14:47:03 +0000

    added function to create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+  * create_file - creates a file
+  * @filename: filename
+  * @text_content: points to text content
+  *
+  * Return: 1 on success or -1 if fails
+  */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, count;
+
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	count = 0;
+	if (!filename || fd == -1)
+		return (-1);
+	while (text_content && text_content[count])
+	{
+		count++;
+	}
+	if (!count)
+		write(fd, "", count);
+	else
+		write(fd, text_content, count);
+	close(fd);
+	return (1);
+}
