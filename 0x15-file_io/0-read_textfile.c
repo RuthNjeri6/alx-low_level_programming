90d9f515a45a22c1b93a3e4dfbe15eed1a4b07ef
rthmbugua@gmail.com <rthmbugua@gmail.com>
2021-03-29 14:28:05 +0000

    fix undeclared variable error

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,7 +11,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fd, b;
 	char *buffer;
 
-	if (!file || letters < 1)
+	if (!filename || letters < 1)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
